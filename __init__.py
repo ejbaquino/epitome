354c59f9bd62cc8f039d3518cbaed5c67d1266e7
Kenneth9997 <kennethlagunilla44@gmail.com>
2018-10-18 11:20:55 +0800

    Added UI for login and admin

--- __init__.py
+++ __init__.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 app.debug = True
 
 @app.route('/')
+@app.route('/home')
 def landing():
     return render_template("index.html")
 
@@ -15,6 +16,30 @@ def login():
 @app.route('/register')
 def register():
     return render_template("register.html")
+
+@app.route('/admin')
+def addItems():
+    return render_template("admin_add_item.html")
+
+@app.route('/admin/books')
+def searchBooks():
+    return render_template("admin_books.html")
+
+@app.route('/admin/category')
+def searchCategory():
+    return render_template("admin_category.html")
+
+@app.route('/admin/users')
+def searchUsers():
+    return render_template("admin_users.html")
+
+@app.route('/admin/books/name')
+def searchBookName():
+    return render_template("admin_view_book.html")
+
+@app.route('/admin/users/name')
+def searchUserName():
+    return render_template("admin_view_user.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
